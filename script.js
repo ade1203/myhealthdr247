0ad9ca44859e94ab2291606f82d405a19f509b43
ade1203 <adebiyipeter367@gmail.com>
2024-11-27 16:02:48 +0100

    new file

---
+++ script.js
@@ -0,0 +1,8 @@
+// Placeholder JavaScript for cookie management and form validation
+document.querySelector('.btn-accept').addEventListener('click', () => {
+  alert('Cookies accepted!');
+});
+
+document.querySelector('.btn-reject').addEventListener('click', () => {
+  alert('Cookies rejected!');
+});
